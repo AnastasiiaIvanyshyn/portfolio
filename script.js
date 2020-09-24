23d1ad0fde4951625a8eca84e66785d8fcb45f84
Anastasiia <45266175+AnastasiiaIvanyshyn@users.noreply.github.com>
2020-09-24 12:56:50 +0300

    Init

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-const navbar = document.querySelector(".navbar");
-navbar.querySelector(".toggle").addEventListener("click", () => {
-  navbar.classList.toggle("collapsed");
-});
-window.addEventListener("scroll", e => {
-  let windowY = window.pageYOffset;
-  let navbarHeight = document.querySelector(".navbar").offsetHeight;
-  if (windowY > navbarHeight) navbar.classList.add("sticky");
-  else navbar.classList.remove("sticky");
-});
+(function () {
 
+    // Definition of caller element
+    var getTriggerElement = function (el) {
+      var isCollapse = el.getAttribute('data-collapse');
+      if (isCollapse !== null) {
+        return el;
+      } else {
+        var isParentCollapse = el.parentNode.getAttribute('data-collapse');
+        return (isParentCollapse !== null) ? el.parentNode : undefined;
+      }
+    };
+  
+    // A handler for click on toggle button
+    var collapseClickHandler = function (event) {
+      var triggerEl = getTriggerElement(event.target);
+      // If trigger element does not exist
+      if (triggerEl === undefined) {
+  
+        return false;
+      }
+  
+      // If the target element exists
+      var targetEl = document.querySelector(triggerEl.getAttribute('data-target'));
+      if (targetEl) {
+        triggerEl.classList.toggle('-active');
+        targetEl.classList.toggle('-on');
+      }
+    };
+  
+    // Delegated event
+    document.addEventListener('click', collapseClickHandler, false);
+  
+  })(document, window);
